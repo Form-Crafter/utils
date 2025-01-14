bb83586b9ab2fb95a35e1c7df8a1b6f19eeed370
Лазука Егор Александрович <ELazuka_in@inno.local>
2025-01-15 00:20:30 +0300

    added types

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export type OptionalKeys<T extends object> = keyof {
+    [K in keyof T as undefined extends T[K] ? K : never]: T[K]
+}
+
+export type RequiredKeys<T extends object> = keyof Omit<T, OptionalKeys<T>>
+
+export type PartialIfUndefined<T extends object> = {
+    [K in keyof T as K extends OptionalKeys<T> ? K : never]?: T[K]
+} & {
+    [K in keyof T as K extends RequiredKeys<T> ? K : never]: T[K]
+}
+
+export type NotUndefinebleKey<T extends Record<string, unknown>, O extends keyof T> = {
+    [K in keyof T]: K extends O ? Exclude<T[K], undefined> : T[K]
+}
