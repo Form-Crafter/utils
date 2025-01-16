0f11867d199ace096b64cc2e6f0399496b0d1bac
Лазука Егор Александрович <ELazuka_in@inno.local>
2025-01-17 00:50:04 +0300

    update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,23 @@
-export type OptionalKeys<T extends object> = keyof {
+export type SomeObject<T = any> = Record<string, T>
+
+export type OptionalKeys<T extends SomeObject> = keyof {
     [K in keyof T as undefined extends T[K] ? K : never]: T[K]
 }
 
-export type RequiredKeys<T extends object> = keyof Omit<T, OptionalKeys<T>>
+export type RequiredKeys<T extends SomeObject> = keyof Omit<T, OptionalKeys<T>>
 
-export type PartialIfUndefined<T extends object> = {
+export type OptionalIfUndefined<T extends SomeObject> = {
     [K in keyof T as K extends OptionalKeys<T> ? K : never]?: T[K]
 } & {
     [K in keyof T as K extends RequiredKeys<T> ? K : never]: T[K]
 }
 
-export type NonUndefinableKey<T extends Record<string, unknown>, O extends keyof T> = {
+export type NonUndefinableKey<T extends SomeObject, O extends keyof T> = {
     [K in keyof T]: K extends O ? Exclude<T[K], undefined> : T[K]
 }
+
+export type Unwrap<T> = T extends object ? { [K in keyof T]: T[K] } : T
+
+export type MakeKeysOptional<T> = T extends SomeObject ? Unwrap<OptionalIfUndefined<T>> : T
+
+export type UniversalValue = string | number | Date | null | { [key: string]: UniversalValue } | UniversalValue[]
